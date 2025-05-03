c94a9cb1de2fe1a15a8f05623646f5d7186031dd
AdhamHaithamEid <adhamhaithameid@gmail.com>
2025-05-03 21:18:37 +0300

    made the essential files for the whether dashboard task

---
+++ Tasks/Whether-Dashboard/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl bg-orange-500 text-center rounded-md m-3 font-bold py-2">
+        hello world
+      </div>
+    </>
+  );
+}
+
+export default App;
